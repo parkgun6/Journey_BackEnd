5471de6734d167757c6d0ac3fb5c9fea8b1eee52
parkgun6 <a01088807068@gmail.com>
2022-04-26 14:48:43 +0900

    Board Mapper Tests

---
+++ src/test/java/org/geon/journey/mapper/BoardMapperTests.java
@@ -0,0 +1,58 @@
+package org.geon.journey.mapper;
+
+import lombok.extern.log4j.Log4j2;
+import org.geon.journey.board.entity.Board;
+import org.geon.journey.board.mapper.BoardMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+@Log4j2
+public class BoardMapperTests {
+
+    @Autowired
+    BoardMapper mapper;
+
+    @Test
+    public void timeNow() {
+        log.info(mapper.getTime());
+    }
+
+    @Test
+    public void getOne() {
+        log.info(mapper.read(1L));
+    }
+
+    @Test
+    public void getList() {
+        mapper.getList(0).forEach(res -> log.info(res));
+    }
+
+    @Test
+    public void insertBoard() {
+        IntStream.rangeClosed(1, 200).forEach(i -> {
+            Board board = Board.builder()
+                    .text("HelloWorld" + i)
+                    .build();
+            mapper.insertBoard(board);
+        });
+    }
+
+    @Test
+    public void update() {
+        Board board = Board.builder()
+                .bno(2L)
+                .text("HelloWorld...수정됨")
+                .build();
+        mapper.update(board);
+    }
+
+    @Test
+    public void delete(){
+        Long bno = 1L;
+        log.info(mapper.delete(bno));
+    }
+}
