7dc252ada04c75783ac8596159d09320713ab2ac
parkgun6 <a01088807068@gmail.com>
2022-04-26 14:48:59 +0900

    Image Mapper Tests

---
+++ src/test/java/org/geon/journey/mapper/ImageMapperTests.java
@@ -0,0 +1,30 @@
+package org.geon.journey.mapper;
+
+import lombok.extern.log4j.Log4j2;
+import org.geon.journey.board.entity.Board;
+import org.geon.journey.board.mapper.ImageMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+@Log4j2
+public class ImageMapperTests {
+
+    @Autowired
+    ImageMapper mapper;
+
+    @Test
+    public void delete(){
+        Long ino = 1L;
+        log.info(mapper.deleteImage(ino));
+    }
+
+    @Test
+    public void deleteByBno(){
+        Long bno = 3L;
+        log.info(mapper.deleteImageByBno(bno));
+    }
+}
