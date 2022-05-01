fde4cf2b8ff10d0cd05f175c69fee799f9745a36
parkgun6 <a01088807068@gmail.com>
2022-05-01 15:01:38 +0900

    feat: getImageList 생성 (수정용)

--- src/main/java/org/geon/journey/board/mapper/ImageMapper.java
+++ src/main/java/org/geon/journey/board/mapper/ImageMapper.java
@@ -3,10 +3,14 @@ package org.geon.journey.board.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.geon.journey.board.entity.Image;
 
+import java.util.List;
+
 @Mapper
 public interface ImageMapper {
 
-    Image insertImage(Image image);
+    Long insertImage(Image image);
+
+    List<Image> getImageList(Long bno);
 
     Long deleteImageByBno(Long bno);
 
