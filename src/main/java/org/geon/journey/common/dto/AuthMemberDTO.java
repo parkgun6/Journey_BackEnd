b2681ed7173661e5f9456c60f67e8856254c3dc8
parkgun6 <a01088807068@gmail.com>
2022-05-02 14:46:48 +0900

    feat: AuthMemberDTO 생성

---
+++ src/main/java/org/geon/journey/common/dto/AuthMemberDTO.java
@@ -0,0 +1,33 @@
+package org.geon.journey.common.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+@ToString
+@Log4j2
+public class AuthMemberDTO extends User {
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private boolean fromSocial;
+
+    public AuthMemberDTO(String username, String password, boolean fromSocial, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+        this.email = username;
+        this.password = password;
+        this.fromSocial = fromSocial;
+    }
+}
