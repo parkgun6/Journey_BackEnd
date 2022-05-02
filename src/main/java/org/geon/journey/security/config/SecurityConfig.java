d90101e9e64dbb4716d2543ae08c79dc25acb510
parkgun6 <a01088807068@gmail.com>
2022-05-02 14:46:11 +0900

    feat: SecurityConfig 생성

---
+++ src/main/java/org/geon/journey/security/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package org.geon.journey.security.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@Log4j2
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    {
+        log.info("security config");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasRole("MEMBER");
+        http.formLogin();
+    }
+
+}
