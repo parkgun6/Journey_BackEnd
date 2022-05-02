e0afc93a1dfa5b9e48601e3f4b918f81c0b69444
parkgun6 <a01088807068@gmail.com>
2022-05-02 14:46:23 +0900

    feat: UserDetailService 생성

---
+++ src/main/java/org/geon/journey/common/service/UserDetailService.java
@@ -0,0 +1,43 @@
+package org.geon.journey.common.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.geon.journey.common.dto.AuthMemberDTO;
+import org.geon.journey.common.entity.Member;
+import org.geon.journey.common.mapper.MemberMapper;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class UserDetailService implements UserDetailsService {
+
+    private final MemberMapper mapper;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("UserDetailService loadUserByUsername : " + username);
+
+        Optional<Member> result = mapper.findByEmail(username, false);
+
+        if(result.isEmpty()){
+            throw new UsernameNotFoundException("Check Email or Social");
+        }
+        Member member = result.get();
+        AuthMemberDTO authMemberDTO = new AuthMemberDTO(
+                member.getEmail(),
+                member.getPassword(),
+                member.isFromSocial(),
+                member.getRoleSet().stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role.name())).collect(Collectors.toSet())
+        );
+        authMemberDTO.setName(member.getName());
+        authMemberDTO.setFromSocial(member.isFromSocial());
+        return authMemberDTO;
+    }
+}
