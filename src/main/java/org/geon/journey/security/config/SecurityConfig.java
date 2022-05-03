db49859144806170530617a8f9e3cd9595b8a268
parkgun6 <a01088807068@gmail.com>
2022-05-03 13:46:29 +0900

    fix: 메인페이지 권한 permitAll로 변경

--- src/main/java/org/geon/journey/security/config/SecurityConfig.java
+++ src/main/java/org/geon/journey/security/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").hasRole("MEMBER");
+                .antMatchers("/").permitAll();
         http.formLogin();
     }
 
