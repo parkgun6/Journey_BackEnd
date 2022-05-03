363cbebdc14350feccd263bd9c3f581b8a92099b
parkgun6 <a01088807068@gmail.com>
2022-05-03 13:46:03 +0900

    feat: registMemberRole Test

--- src/test/java/org/geon/journey/common/mapper/MemberMapperTests.java
+++ src/test/java/org/geon/journey/common/mapper/MemberMapperTests.java
@@ -2,11 +2,13 @@ package org.geon.journey.common.mapper;
 
 import lombok.extern.log4j.Log4j2;
 import org.geon.journey.common.entity.Member;
+import org.geon.journey.common.entity.MemberRole;
 import org.geon.journey.common.mapper.MemberMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -24,22 +26,34 @@ public class MemberMapperTests {
 
     @Test
     public void insertMembers() {
-        IntStream.rangeClosed(1,100).forEach(i -> {
+        IntStream.rangeClosed(1,5).forEach(i -> {
             Member member = Member.builder()
                     .email("user"+i+"@journey.com")
                     .name("user"+i)
                     .password(passwordEncoder.encode("1234"))
                     .build();
+            member.addMemberRole(MemberRole.USER);
+
+            if(i > 80){
+                member.addMemberRole(MemberRole.MEMBER);
+            }
+
+            if(i > 90){
+                member.addMemberRole(MemberRole.ADMIN);
+            }
+            log.info(member.getRoleSet());
+            log.info(member.getRoleSet());
+            log.info(member.getRoleSet());
             mapper.registMember(member);
+//            mapper.registMemberRole(member);
         });
     }
 
     @Test
     public void selectMember() {
-        Optional<Member> result = mapper.findByEmail("user100@journey.com",false);
+        Optional<Member> result = mapper.findByEmail("user1@journey.com",false);
 
-        Member member = result.get();
-        log.info(member.getRoleSet());
-        log.info(member.getRole());
+        log.info(result.get().getEmail());
+        log.info(result.get().getRoleSet());
     }
 }
