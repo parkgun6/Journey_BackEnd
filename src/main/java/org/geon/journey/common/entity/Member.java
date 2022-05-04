1deefdcfd8a2a6dfacff6c375a689f81800b3cb8
parkgun6 <a01088807068@gmail.com>
2022-05-04 17:30:53 +0900

    feat: Member Entity 생성

---
+++ src/main/java/org/geon/journey/common/entity/Member.java
@@ -0,0 +1,36 @@
+package org.geon.journey.common.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private boolean fromSocial;
+
+    @Builder.Default
+    private Set<MemberRole> roleSet = new HashSet<>();
+
+    public void addMemberRole(MemberRole memberRole){
+        roleSet.add(memberRole);
+    }
+
+    private Date regDate;
+
+    private Date modDate;
+}
