c7d9d888601ba687ae157539ceb8bd62ff2ecedb
parkgun6 <a01088807068@gmail.com>
2022-05-08 21:35:32 +0900

    feat: getReplys 생성

--- src/main/java/org/geon/journey/common/mapper/ReplyMapper.java
+++ src/main/java/org/geon/journey/common/mapper/ReplyMapper.java
@@ -1,10 +1,15 @@
 package org.geon.journey.common.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.geon.journey.common.entity.Reply;
+
+import java.util.List;
 
 @Mapper
 public interface ReplyMapper {
 
     void insertReply(Reply reply);
+
+    List<Reply> getReplys(@Param("page") int page);
 }
