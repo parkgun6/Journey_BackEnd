7a9733428b23a15ea4220dc3f18f60c2c5658a57
parkgun6 <a01088807068@gmail.com>
2022-05-08 21:35:03 +0900

    feat: entityToDTO, getReplys생성

--- src/main/java/org/geon/journey/common/service/ReplyService.java
+++ src/main/java/org/geon/journey/common/service/ReplyService.java
@@ -1,4 +1,25 @@
 package org.geon.journey.common.service;
 
+import org.geon.journey.common.dto.ReplyDTO;
+import org.geon.journey.common.entity.Reply;
+
+import java.util.List;
+
 public interface ReplyService {
+
+    void insertReply(Reply reply);
+
+    List<ReplyDTO> getReplys(int page);
+
+    default ReplyDTO entityToDTO(Reply reply){
+        ReplyDTO replyDTO = ReplyDTO.builder()
+                .rno(reply.getRno())
+                .bno(reply.getBno())
+                .email(reply.getEmail())
+                .reply(reply.getReply())
+                .regDate(reply.getRegDate())
+                .build();
+
+        return replyDTO;
+    }
 }
--- src/main/java/org/geon/journey/common/service/impl/ReplyServiceImpl.java
+++ src/main/java/org/geon/journey/common/service/impl/ReplyServiceImpl.java
@@ -2,9 +2,14 @@ package org.geon.journey.common.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.geon.journey.common.dto.ReplyDTO;
+import org.geon.journey.common.entity.Reply;
 import org.geon.journey.common.mapper.ReplyMapper;
 import org.geon.journey.common.service.ReplyService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Log4j2
@@ -12,4 +17,15 @@ import org.springframework.stereotype.Service;
 public class ReplyServiceImpl implements ReplyService {
 
     private final ReplyMapper replyMapper;
+
+    @Override
+    public void insertReply(Reply reply) {
+        replyMapper.insertReply(reply);
+    }
+
+    @Override
+    public List<ReplyDTO> getReplys(int page) {
+        List<Reply> reply = replyMapper.getReplys(page);
+        return reply.stream().map(res -> entityToDTO(res)).collect(Collectors.toList());
+    }
 }
