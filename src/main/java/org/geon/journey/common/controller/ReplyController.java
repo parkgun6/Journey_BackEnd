72b3ac50a90e1f9f2fd915b4d47be1a3a3a6acf1
parkgun6 <a01088807068@gmail.com>
2022-05-08 21:36:09 +0900

    feat: getReplys 생성

--- src/main/java/org/geon/journey/common/controller/ReplyController.java
+++ src/main/java/org/geon/journey/common/controller/ReplyController.java
@@ -2,9 +2,16 @@ package org.geon.journey.common.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.geon.journey.common.dto.ReplyDTO;
 import org.geon.journey.common.service.ReplyService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Log4j2
@@ -13,4 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReplyController {
 
     private final ReplyService replyService;
+
+    @GetMapping("/replys/{page}")
+    public ResponseEntity<List<ReplyDTO>> getReplys(@PathVariable("page") int page) {
+        int pageNum = (page - 1) * 2;
+        log.info(pageNum);
+        return new ResponseEntity<>(replyService.getReplys(pageNum), HttpStatus.OK);
+    }
 }
